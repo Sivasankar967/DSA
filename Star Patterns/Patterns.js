e4d9b88b7ddb68ea8db11923c8629022612691ca
SivaSankar <76865387+Sivasankar967@users.noreply.github.com>
2025-07-22 20:28:19 +0530

    Basic Problems

---
+++ Star Patterns/Patterns.js
@@ -0,0 +1,37 @@
+// let n = 4;
+
+// for (let i = 0; i < n; i++) {
+//     let row = ""
+//     for (let j = 0; j < n; j++) {
+//         row = row + "*"
+//     }
+//     console.log(row)
+// }
+
+// let n = 4;
+// for (let i = 0; i < n + 1; i++) {
+//     let row = "";
+//     for (let j = 0; j < i; j++) {
+//         row = row + "*";
+//     }
+//     console.log(row)
+// }
+
+// let n = 5;
+// for (let i = 0; i < n; i++) {
+//     let row = "";
+//     for (j = 0; j <= i; j++) {
+//         row = row + (j + 1);
+//     }
+//     console.log(row)
+// }
+
+
+
+
+
+
+
+
+
+
