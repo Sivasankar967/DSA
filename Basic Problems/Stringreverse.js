5e4bdf7aa5ece1da1068b72c70bc645fc1207267
SivaSankar <76865387+Sivasankar967@users.noreply.github.com>
2025-08-07 07:46:55 +0530

    String Reverse

--- Basic Problems/Stringreverse.js
+++ Basic Problems/Stringreverse.js
@@ -1,6 +1,13 @@
-let str="siva";
-let reverse="";
-for(i=str.length-1;i>0;i--){
-    reverse+=str[i]
-}
-console.log(reverse)
+let s = ["s", "i", "v", "a"];
+var reverseString = function (s) {
+    let len = s.length;
+    let halflen = Math.floor(len / 2);
+    for (let i = 0; i < halflen; i++) {
+        let temp = s[i];
+        s[i] = s[len - 1 - i];
+        s[len - 1 - i] = temp
+    }
+    return s
+};
+let Result = reverseString(s);
+console.log(Result)
